047d80353817c6bc4e7efbe7def5036643712d88
luthfi2603 <luthfim904@gmail.com>
2023-11-17 20:52:33 +0700

    menampilkan jadwal per ruangan dan hari

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
-import com.config.Config;
+import com.view.View;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Config.connection();
+        View.index();
     }
 }
--- src/com/config/Config.java
+++ src/com/config/Config.java
@@ -13,13 +13,49 @@ public class Config {
     private static Statement statement;
     private static ResultSet result;
 
-    public static void connection(){
+    private static void connection(){
         try {
             connect = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-
-            System.out.println("Koneksi Berhasil");
         }catch(Exception e){
             System.out.println("Error : " + e.getMessage());
         }
+    }
+
+    public static String getJadwalPerHariDanRuangan(String hari, String ruangan){
+        connection();
+
+        String data = "";
+
+        try {
+            statement = connect.createStatement();
+            String query = "SELECT * FROM gabungan WHERE hari = '" + hari + "' AND kode_ruangan = '" + ruangan + "' ORDER BY hari, jam";
+
+            result = statement.executeQuery(query);
+
+            while(result.next()){
+                data += result.getString(1) + " "
+                + result.getString(2) + " "
+                + result.getString(3) + " "
+                + result.getString(4) + " "
+                + result.getString(5) + " "
+                + result.getString(6) + " "
+                + result.getString(7) + " "
+                + result.getString(8) + " "
+                + result.getString(9) + " "
+                + result.getString(10) + " "
+                + result.getString(11);
+            }
+
+            if(data == ""){
+                data = "Kosong";
+            }
+
+            statement.close();
+            connect.close();
+        }catch(Exception e){
+            System.out.println("Error : " + e.getMessage());
+        }
+
+        return data;
     }
 }
