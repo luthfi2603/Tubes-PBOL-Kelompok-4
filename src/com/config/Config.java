8e10008a2c59f5565d7b0a1879576fd578accb69
luthfi2603 <luthfim904@gmail.com>
2023-11-18 09:41:28 +0700

    membuat menu cli, mendapatkan current timestamp, jadwal angkatan 22

--- src/com/config/Config.java
+++ src/com/config/Config.java
@@ -3,6 +3,8 @@ import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Config {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/tubes_pbol";
@@ -12,6 +14,10 @@ public class Config {
     private static Connection connect;
     private static Statement statement;
     private static ResultSet result;
+
+    LocalDateTime currentTimestamp = LocalDateTime.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE HH:mm");
+    String formattedTimestamp = currentTimestamp.format(formatter);
 
     private static void connection(){
         try {
@@ -33,21 +39,59 @@ public class Config {
             result = statement.executeQuery(query);
 
             while(result.next()){
-                data += result.getString(1) + " "
-                + result.getString(2) + " "
-                + result.getString(3) + " "
-                + result.getString(4) + " "
-                + result.getString(5) + " "
-                + result.getString(6) + " "
-                + result.getString(7) + " "
-                + result.getString(8) + " "
-                + result.getString(9) + " "
-                + result.getString(10) + " "
-                + result.getString(11);
+                data += result.getString(1) + " | "
+                + result.getString(2) + " | "
+                + result.getString(3) + " | "
+                + result.getString(4) + " | "
+                + result.getString(5) + " | "
+                + result.getString(6) + " | "
+                + result.getString(7) + " | "
+                + result.getString(8) + " | "
+                + result.getString(9) + " | "
+                + result.getString(10) + " | "
+                + result.getString(11) + "\n";
             }
 
             if(data == ""){
-                data = "Kosong";
+                data = "Kosong\n";
+            }
+
+            statement.close();
+            connect.close();
+        }catch(Exception e){
+            System.out.println("Error : " + e.getMessage());
+        }
+
+        return data;
+    }
+
+    public static String getJadwalSaatIni(String hari, String ruangan){
+        connection();
+
+        String data = "";
+
+        try {
+            statement = connect.createStatement();
+            String query = "SELECT semester, kom, kode_matkul, kode_dosen FROM gabungan WHERE hari = '" + hari + "' AND kode_ruangan = '" + ruangan + "' ORDER BY hari, jam";
+
+            result = statement.executeQuery(query);
+
+            while(result.next()){
+                data += result.getString(1) + " | "
+                + result.getString(2) + " | "
+                + result.getString(3) + " | "
+                + result.getString(4) + " | "
+                + result.getString(5) + " | "
+                + result.getString(6) + " | "
+                + result.getString(7) + " | "
+                + result.getString(8) + " | "
+                + result.getString(9) + " | "
+                + result.getString(10) + " | "
+                + result.getString(11) + "\n";
+            }
+
+            if(data == ""){
+                data = "Kosong\n";
             }
 
             statement.close();
--- src/com/view/View.java
+++ src/com/view/View.java
@@ -1,8 +1,64 @@
 package com.view;
+import java.util.Scanner;
 import com.config.Config;
 
 public class View {
+    private static Scanner input = new Scanner(System.in);
+    private static String pilihan, ruangan, hari;
+
     public static void index(){
+        while(true){
+            System.out.print("\n========== MENU ==========\n"
+            + "1. Lihat Status Kelas Saat Ini\n"
+            + "2. Detail Kelas Perhari\n"
+            + "0. Exit\n"
+            + "Pilih [1/2/3/4/5/0] : ");
+            pilihan = input.nextLine();
+
+            switch(pilihan){
+                case "1":
+                    getJadwalSaatIni();
+                    break;
+                case "2":
+                    getJadwalPerHariDanRuangan();
+                    break;
+                case "0":
+                    System.out.println("\nExit");
+                    break;
+                default:
+                    System.out.println("Pilihan anda invalid!");
+                    break;
+            }
+
+            if(pilihan.equalsIgnoreCase("0")){
+                break;
+            }
+        }
+    }
+
+    public static void getJadwalPerHariDanRuangan(){
+        System.out.println("\n::: Detail Kelas Perhari :::");
+        System.out.print("Ruangan : ");
+        ruangan = input.nextLine();
+        System.out.print("Hari : ");
+        hari = input.nextLine();
+
+        System.out.println("\nRuangan : " + ruangan + ", Hari : " + hari);
+        System.out.print(Config.getJadwalPerHariDanRuangan(hari, ruangan));
+    }
+
+    public static void getJadwalSaatIni(){
+        System.out.println("\n::: Halaman Utama :::");
+        System.out.println("\nC-101\n" + "=====");
+        System.out.println("\nC-102\n" + "=====");
+        System.out.println("\nC-103\n" + "=====");
+        System.out.println("\nC-104\n" + "=====");
+        System.out.println("\nC-105\n" + "=====");
+        System.out.println("\nC-106\n" + "=====");
+        System.out.println();
+    }
+
+    /* public static void index2(){
         System.out.println("\nTubes PBOL Kelompok 4");
         System.out.println("\nC-101\n" + "=====\n");
         System.out.println("Senin" + "\n------");
@@ -70,5 +126,5 @@ public class View {
         System.out.println(Config.getJadwalPerHariDanRuangan("Kamis", "C-106"));
         System.out.println("\nJumat" + "\n------");
         System.out.println(Config.getJadwalPerHariDanRuangan("Jumat", "C-106"));
-    }
+    } */
 }
