233a750519d9e06056a6057fb32f68bb6fc30efb
luthfi2603 <luthfim904@gmail.com>
2023-11-22 22:22:10 +0700

    halaman sudah terhubung dengan tombol

--- src/App.java
+++ src/App.java
@@ -1,7 +1,34 @@
-import com.view.View;
+// import com.view.View;
+
+import com.view.Home;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        View.index();
+        // View.index();
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Home.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Home.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Home.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Home.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                Home home = new Home();
+                home.setLocationRelativeTo(null);
+                home.setVisible(true);
+            }
+        });
     }
 }
